dc7f7684acc6b3e667a56c12e0d615d8dd816fba
Job Doesburg <job.doesburg@gmail.com>
2020-07-27 17:38:09 +0200

    Media app

--- website/landolfio/urls.py
+++ website/landolfio/urls.py
@@ -2,14 +2,16 @@ from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.urls import path
 from django.views.static import serve
 
 
 @login_required(redirect_field_name="next", login_url="admin:login")
 def protected_serve(request, path, document_root=None, show_indexes=True):
-    if request.user.is_authenticated and request.user.has_perm("media.view_mediaitem"):
+    if request.user.is_authenticated and request.user.has_perm("asset_media.view_mediaitem"):
         return serve(request, path, document_root, show_indexes)
+    raise PermissionDenied
 
 
 urlpatterns = [
