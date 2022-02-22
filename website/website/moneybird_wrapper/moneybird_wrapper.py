a1db761f550fb154a779b498b816227a25d971da
JasperJH <JasperJH@users.noreply.github.com>
2022-02-22 14:30:45 +0100

    Implement basic wrapper

---
+++ website/website/moneybird_wrapper/moneybird_wrapper.py
@@ -0,0 +1,66 @@
+"""
+Moneybird API wrapper
+"""
+from moneybird import MoneyBird, TokenAuthentication
+
+
+class MoneyBirdWrapper:
+    """
+    A Moneybird API wrapper
+    """
+
+    def __init__(self, key: str):
+        """
+        Initialize the Moneybird Wrapper
+
+        :key: the API key
+        """
+        self.api = MoneyBird(TokenAuthentication(key))
+        self.administration_id = self.api.get("administrations")[0]["id"]
+
+    def load_documents(self, document_type: str):
+        """
+        Loads the documents of the specified type
+
+        :param document_type: the type of document we want to load
+        :return: requested documents as JSON
+        """
+        documents = self.api.get(
+            f"documents/{document_type}/synchronization",
+            administration_id=self.administration_id,
+        )
+        # We can request at most 100 documents in one go
+        split = [documents[x : x + 100] for x in range(0, len(documents), 100)]
+
+        return_data = []
+        for split_documents in split:
+            ids = []
+
+            # Load all the ids
+            for document in split_documents:
+                ids.append(document["id"])
+
+            # Prepare the ids dictionary
+            request_ids = {"ids": ids}
+            requested_documents = self.api.post(
+                f"documents/{document_type}/synchronization",
+                data=request_ids,
+                administration_id=self.administration_id,
+            )
+            return_data.extend(requested_documents)
+
+        return return_data
+
+    def load_purchase_invoices(self):
+        """
+        Loads purchase invoices
+        :return: purchase invoices as JSON
+        """
+        return self.load_documents("purchase_invoices")
+
+    def load_receipts(self):
+        """
+        Loads receipts
+        :return: receipts as JSON
+        """
+        return self.load_documents("receipts")
