59af985dbb07fd56ac75c549c9364b417f169403
JasperJH <JasperJH@users.noreply.github.com>
2022-03-07 15:34:33 +0100

    Increase line coverage with tests

--- website/website/moneybird_wrapper/test_moneybird_wrapper.py
+++ website/website/moneybird_wrapper/test_moneybird_wrapper.py
@@ -3,44 +3,61 @@ import unittest
 from unittest.mock import patch
 
 
-class MoneyBirdMock:
-    """Mock version of the MoneyBird API."""
+def make_moneybird_mock(purchase_invoices, receipts):
+    """Create a Mock version of the Moneybird API with given purchase invoices and receipts."""
 
-    # pylint: disable=too-few-public-methods
+    class MoneyBirdMock:
+        """Mock version of the MoneyBird API."""
 
-    def __init__(self, authentication):
-        """Initialize a new MoneyBirdMock."""
-        self.authentication = authentication
+        # pylint: disable=too-few-public-methods
 
-    def get(self, resource_path: str, administration_id: int = None):
-        """Mock a GET request for the Moneybird API."""
-        # pylint: disable=unused-argument,no-self-use
-        if resource_path == "administrations":
-            return [{"id": "0"}]
+        def __init__(self, authentication):
+            """Initialize a new MoneyBirdMock."""
+            self.authentication = authentication
+            self.purchase_invoices = purchase_invoices
+            self.receipts = receipts
 
-        if resource_path == "documents/purchase_invoices/synchronization":
-            return {}
+        def get(self, resource_path: str, administration_id: int = None):
+            """Mock a GET request for the Moneybird API."""
+            # pylint: disable=unused-argument
+            if resource_path == "administrations":
+                return [{"id": "0"}]
 
-        if resource_path == "documents/receipts/synchronization":
-            return {}
+            if resource_path == "documents/purchase_invoices/synchronization":
+                return [
+                    {"id": invoice["id"], "version": invoice["version"]}
+                    for invoice in self.purchase_invoices
+                ]
 
-        # Incorrect path was used
-        raise MoneyBirdMock.NotFound
+            if resource_path == "documents/receipts/synchronization":
+                return [
+                    {"id": receipt["id"], "version": receipt["version"]}
+                    for receipt in self.receipts
+                ]
 
-    def post(self, resource_path: str, data: dict, administration_id: int = None):
-        """Mock a POST request for the Moneybird API."""
-        # pylint: disable=unused-argument,no-self-use
-        if resource_path == "documents/purchase_invoices/synchronization":
-            return {}
+            # Incorrect path was used
+            raise MoneyBirdMock.NotFound
 
-        if resource_path == "documents/receipts/synchronization":
-            return {}
+        def post(self, resource_path: str, data: dict, administration_id: int = None):
+            """Mock a POST request for the Moneybird API."""
+            # pylint: disable=unused-argument
+            if resource_path == "documents/purchase_invoices/synchronization":
+                ids = data["ids"]
+                return filter(
+                    lambda invoice: invoice["id"] in ids, self.purchase_invoices
+                )
 
-        # Incorrect path was used
-        raise MoneyBirdMock.NotFound
+            if resource_path == "documents/receipts/synchronization":
+                ids = data["ids"]
+                return filter(lambda receipt: receipt["id"] in ids, self.receipts)
 
-    class NotFound(Exception):
-        """Exception for requests to non-existing resource paths."""
+            # Incorrect path was used
+            raise MoneyBirdMock.NotFound
+
+        class NotFound(Exception):
+            """Exception for requests to non-existing resource paths."""
+
+    return MoneyBirdMock
 
 
 class MoneybirdWrapperTest(unittest.TestCase):
@@ -48,24 +65,34 @@ class MoneybirdWrapperTest(unittest.TestCase):
 
     def test_load_purchase_invoices(self):
         """Test loading purchase invoices."""
-        with patch("moneybird.MoneyBird", new=MoneyBirdMock):
+        purchase_invoices = [{"id": "1", "version": 3}, {"id": "2", "version": 7}]
+        receipts = [{"id": "112", "version": 2}, {"id": "22", "version": 5}]
+        with patch(
+            "moneybird.MoneyBird", new=make_moneybird_mock(purchase_invoices, receipts)
+        ):
             # pylint: disable=import-outside-toplevel
             from .moneybird_wrapper import MoneyBirdWrapper
 
             wrapper = MoneyBirdWrapper("mock_key")
             self.assertListEqual(
                 wrapper.load_purchase_invoices(),
-                [],
+                purchase_invoices,
                 "Incorrect purchase invoices response",
             )
 
     def test_load_receipts(self):
         """Test loading receipts."""
-        with patch("moneybird.MoneyBird", new=MoneyBirdMock):
+        purchase_invoices = [{"id": "1", "version": 3}, {"id": "2", "version": 7}]
+        receipts = [{"id": "112", "version": 2}, {"id": "22", "version": 5}]
+        with patch(
+            "moneybird.MoneyBird", new=make_moneybird_mock(purchase_invoices, receipts)
+        ):
             # pylint: disable=import-outside-toplevel
             from .moneybird_wrapper import MoneyBirdWrapper
 
             wrapper = MoneyBirdWrapper("mock_key")
             self.assertListEqual(
-                wrapper.load_receipts(), [], "Incorrect receipts response"
+                wrapper.load_receipts(),
+                receipts,
+                "Incorrect receipts response",
             )
