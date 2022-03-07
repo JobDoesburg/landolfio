27466bbb441101c5a4db2db7d55766e41c0b9e5d
Luuk Maas <maas.luuk@gmail.com>
2022-03-07 14:16:36 +0100

    Asset test case

--- website/assets/test_models.py
+++ website/assets/test_models.py
@@ -1,20 +1,53 @@
+import datetime
+
 from django.test import TestCase
-from .models import Asset, AssetState
 
+from .models import Asset
+from .models import AssetState
 
+# pylint: disable=no-member
 class AssetTestCase(TestCase):
+    """Test cases for the Asset model."""
+
     def setUp(self):
-        Asset.objects.create(old_id="C7801", asset_type="Violin", size="7/8", collection="Zakelijk", listing_price="750", stock_price="1200", purchasing_value="200", margin=True)
+        """Set up the test case."""
+        asset = Asset.objects.create(
+            old_id="C7801",
+            asset_type="Violin",
+            size="7/8",
+            collection="Zakelijk",
+            listing_price="750",
+            stock_price="1200",
+            purchasing_value="200",
+            margin=True,
+        )
+        AssetState.objects.create(
+            asset=asset,
+            date=datetime.date(2022, 1, 25),
+            state="purchased",
+            room="Room A",
+            closet="Closet B",
+            external="External C",
+        )
 
     def test_assets_attributes(self):
-        """Test the functionality of an asset"""
-
+        """Test the functionality of an asset."""
         asset = Asset.objects.get(old_id="C7801")
-        self.assertEqual(asset.old_id, 'C7801')
-        self.assertEqual(asset.asset_type, 'Violin')
-        self.assertEqual(asset.size, '7/8')
-        self.assertEqual(asset.collection, 'Zakelijk')
+        self.assertEqual(asset.old_id, "C7801")
+        self.assertEqual(asset.asset_type, "Violin")
+        self.assertEqual(asset.size, "7/8")
+        self.assertEqual(asset.collection, "Zakelijk")
         self.assertEqual(asset.listing_price, 750)
         self.assertEqual(asset.stock_price, 1200)
         self.assertEqual(asset.purchasing_value, 200)
         self.assertEqual(asset.margin, True)
+
+    def test_asset_state_relation(self):
+        """Test ability to retrieve AssetStates from Asset."""
+        asset = Asset.objects.get(old_id="C7801")
+        assetState = AssetState.objects.get(asset=asset)
+        self.assertEqual(assetState.date, datetime.date(2022, 1, 25))
+        self.assertEqual(assetState.state, "purchased")
+        self.assertEqual(assetState.room, "Room A")
+        self.assertEqual(assetState.closet, "Closet B")
+        self.assertEqual(assetState.external, "External C")
