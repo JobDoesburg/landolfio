8c81baa24800e4d87c74bbaf1323829274b0f4d9
Stef Gijsberts <55096788+Stef-Gijsberts@users.noreply.github.com>
2022-04-13 14:50:37 +0200

    Extend the accounting app's doc-comment
    To explain what Django settings are needed.

--- website/accounting/__init__.py
+++ website/accounting/__init__.py
@@ -1,1 +1,7 @@
-"""The part of Landolfio concerning accounting."""
+"""
+The part of Landolfio concerning accounting.
+
+This app expects the following Django settings to be set:
+ - MONEYBIRD_API_KEY
+ - MONEYBIRD_ADMINISTRATION_ID
+"""
