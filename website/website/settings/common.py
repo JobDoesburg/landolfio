42f005cb7dad60c32d86d7b3dec91720dd32148c
Stef Gijsberts <55096788+Stef-Gijsberts@users.noreply.github.com>
2022-04-21 18:00:19 +0200

    Specify the media root just like the static root
    And add a gitignore entry for it.

--- website/website/settings/common.py
+++ website/website/settings/common.py
@@ -88,6 +88,8 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = BASE_DIR / "static"
 
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
