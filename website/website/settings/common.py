477b155fabd0b6280e68c2f7b8cb36396034165f
Stef Gijsberts <55096788+Stef-Gijsberts@users.noreply.github.com>
2022-04-27 22:04:18 +0200

    Set the internationalization correctly
    This should have been done a long time ago.

--- website/website/settings/common.py
+++ website/website/settings/common.py
@@ -74,9 +74,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "nl-nl"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Amsterdam"
 
 USE_I18N = True
 
