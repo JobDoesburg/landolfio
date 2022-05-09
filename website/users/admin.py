8736dca8fa19ea01731855ed5ab8ae467cec62da
Stef Gijsberts <55096788+Stef-Gijsberts@users.noreply.github.com>
2022-05-09 14:49:53 +0200

    Use the default UserAdmin for the User model
    To make sure the User section of the admin interface works like it does
    by default in Django.

--- website/users/admin.py
+++ website/users/admin.py
@@ -1,8 +1,9 @@
 """Users admin configuration."""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 
 from .models import User
 
 admin.site.unregister(Group)
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
