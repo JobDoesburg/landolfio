6a52bba3455045cab4aaee4a89b1f07c2c163a9b
Stef Gijsberts <55096788+Stef-Gijsberts@users.noreply.github.com>
2022-05-24 22:00:14 +0200

    Change the Documents Admin to inline DocumentLines

--- website/accounting/admin.py
+++ website/accounting/admin.py
@@ -4,5 +4,18 @@ from django.contrib import admin
 from .models import Document
 from .models import DocumentLine
 
-admin.site.register(Document)
-admin.site.register(DocumentLine)
+
+class DocumentLineAdmin(admin.TabularInline):
+    """The admin view for DocumentLines."""
+
+    model = DocumentLine
+    fields = ("asset_id_field", "asset", "ledger")
+
+
+class DocumentAdmin(admin.ModelAdmin):
+    """The admin view for Documents."""
+
+    inlines = (DocumentLineAdmin,)
+
+
+admin.site.register(Document, DocumentAdmin)
