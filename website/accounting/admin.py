a28547db46d1c90d4d309f44d215c70927b44e92
Luuk Maas <maas.luuk@gmail.com>
2022-05-27 12:27:38 +0200

    Convert JSON to HTML in admin

--- website/accounting/admin.py
+++ website/accounting/admin.py
@@ -1,8 +1,41 @@
 """Accounting admin configuration."""
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+from json2html import json2html
 
 from .models import Document
 from .models import DocumentLine
 
-admin.site.register(Document)
-admin.site.register(DocumentLine)
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    """
+    The admin class for the Document model.
+    """
+
+    model = Document
+    fields = ["id_MB", "json_mb_html", "kind"]
+    readonly_fields = ["json_mb_html"]
+
+    def json_mb_html(self, obj):  # pylint: disable = no-self-use
+        """Convert JSON to HTML table."""
+        return mark_safe(json2html.convert(obj.json_MB))
+
+    json_mb_html.short_description = "JSON MoneyBird"
+
+
+@admin.register(DocumentLine)
+class DocumentLineAdmin(admin.ModelAdmin):
+    """
+    The admin class for the DocumentLine model.
+    """
+
+    model = DocumentLine
+    fields = ["json_mb_html", "ledger", "document", "asset_id_field", "asset"]
+    readonly_fields = ["json_mb_html"]
+
+    def json_mb_html(self, obj):  # pylint: disable = no-self-use
+        """Convert JSON to HTML table."""
+        return mark_safe(json2html.convert(obj.json_MB))
+
+    json_mb_html.short_description = "JSON MoneyBird"
