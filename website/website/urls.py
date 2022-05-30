5971a711d3abc78fbea6ba766aec902e1756b352
Tom Rust @laptop <tomrusteze@gmail.com>
2022-05-30 11:57:25 +0200

    Add media to urls.py

--- website/website/urls.py
+++ website/website/urls.py
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
 from django.urls import re_path
 from django.views.generic.base import RedirectView
 
-urlpatterns = [
+# Static files are NOT authenticated and should be replaced by files in an S3 bucket.
+urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
     re_path("api/accounting/", include("accounting.api.urls")),
     path("admin/", admin.site.urls),
     # The admin interface is the only thing we serve, so just always redirect
