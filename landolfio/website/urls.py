370289d8e7c8710d44accbd143a5f352f258f944
Job Doesburg <job.doesburg@gmail.com>
2022-07-28 00:46:15 +0200

    Fix letsencrypt

--- landolfio/website/urls.py
+++ landolfio/website/urls.py
@@ -32,6 +32,6 @@ urlpatterns = (
         path("api/accounting/", include("accounting.api.urls")),
         re_path(r"^media/", views.protected_ask_reverse_proxy),
         # Always redirect to the admin interface as a fallback
-        path("/", RedirectView.as_view(url="/admin/")),
+        re_path(r"^.*", RedirectView.as_view(url="/admin/")),
     ]
 )
