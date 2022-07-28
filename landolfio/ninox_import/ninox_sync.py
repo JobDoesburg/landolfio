cd9936c2aca80b5dd1ccf6f9c4675a32c6a965bf
Job Doesburg <job.doesburg@gmail.com>
2022-07-28 13:05:55 +0200

    Fix constraint in ninox sync

--- landolfio/ninox_import/ninox_sync.py
+++ landolfio/ninox_import/ninox_sync.py
@@ -93,8 +93,8 @@ class NinoxImporter:
 
     ninox_collection_to_collection = {
         "Zakelijk": Collection.objects.get_or_create(name="Zakelijk", commerce=True)[0],
-        "Prive": Collection.objects.get_or_create(name="Prive")[0],
-        "Consignatie": Collection.objects.get_or_create(name="Consignatie")[0],
+        "Prive": Collection.objects.get_or_create(name="Prive", commerce=False)[0],
+        "Consignatie": Collection.objects.get_or_create(name="Consignatie", commerce=False)[0],
         "Zakelijk (S)": Collection.objects.get_or_create(
             name="Zakelijk (S)", commerce=True
         )[0],
