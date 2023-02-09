e6b5621dcd0e2f28a5a4620dd7770131729fdd86
Job Doesburg <job.doesburg@gmail.com>
2023-02-09 11:36:59 +0100

    Vertalingen

--- landolfio/tickets/apps.py
+++ landolfio/tickets/apps.py
@@ -5,5 +5,5 @@ from django.utils.translation import gettext_lazy as _
 class TicketsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "tickets"
-    verbose_name = _("Tickets")
+    verbose_name = _("📝 Tickets")
     default = True
