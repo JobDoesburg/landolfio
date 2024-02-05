0a2e5d8d4d8da61796ec54dcd3e055940e462ceb
Job Doesburg <job.doesburg@gmail.com>
2024-02-05 22:51:43 +0100

    Fix produ

--- landolfio/website/settings/production.py
+++ landolfio/website/settings/production.py
@@ -29,7 +29,7 @@ if SENTRY_DSN:
 SECRET_KEY = os.environ.get("LANDOLFIO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("LANDOLFIO_ALLOWED_HOSTS", "").split(",")
 
