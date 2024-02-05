bafc5d08c29388f777eb610ff5d80c9c860c81f9
Job Doesburg <job.doesburg@gmail.com>
2024-02-05 22:54:21 +0100

    Fix produ

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 python manage.py migrate --noinput
-uwsgi --http :80 --wsgi-file /app/website/wsgi.py --master --processes 4 --threads 2 --uid nobody --gid nogroup --static-map ${DJANGO_STATIC_URL}=${DJANGO_STATIC_ROOT} --static-map ${DJANGO_MEDIA_URL}=${DJANGO_MEDIA_ROOT}
+uwsgi --http :80 --wsgi-file /app/website/wsgi.py --master --processes 4 --threads 2 --uid nobody --gid nogroup --static-map ${DJANGO_STATIC_URL}=${DJANGO_STATIC_ROOT} --static-map ${DJANGO_MEDIA_URL}=${DJANGO_MEDIA_ROOT} --logto /var/log/uwsgi.log
