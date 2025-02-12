36a17782f9daccc35e3fffc6fbd50c5370af4acb
Job Doesburg <job.doesburg@gmail.com>
2025-02-12 20:39:34 +0100

    Fixes

--- landolfio/website/urls.py
+++ landolfio/website/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
-from django.urls import re_path
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
